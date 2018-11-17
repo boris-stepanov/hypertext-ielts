d3efc2e64ffeca86b6c9559613dedd7f0b5c90fc
Boris Stepanov <boris.stepanov@protonmail.com>
2018-11-17 23:06:00 +0300

    Beaturify init script

--- init.py
+++ init.py
@@ -11,52 +11,61 @@ from source.model.user import UserLogin
 from source.model.exercise import Exercise, Task, Context
 
 
-content = load(open("state.yaml"))
+def init():
+    db.create_all()
 
-db.create_all()
+def gen_students(students):
+    db.session.add(UserLogin("root", "", "redhothotie"))
+    students = open("students.csv", "w")
+    for l in students:
+        line = l.strip()
+        print(line)
+        if line[:2] == "__":
+            group = line[2:]
+            students.write("{}\n\n".format(group))
+        elif line:
+            login = "user_" + md5(line.encode('utf8')).hexdigest()[:8]
+            password = ''.join(choices(ascii_letters + digits, k=8))
+            db.session.add(UserLogin(login, group, password))
+            students.write("{}\t{}\t{}\n".format(login, password, line))
+    db.session.commit()
 
-db.session.add(UserLogin("root", "", "redhothotie"))
-students = open("students.csv", "w")
-for l in open("students.txt"):
-    line = l.strip()
-    print(line)
-    if line[:2] == "__":
-        group = line[2:]
-        students.write("{}\n\n".format(group))
-    elif line:
-        login = "user_" + md5(line.encode('utf8')).hexdigest()[:8]
-        password = ''.join(choices(ascii_letters + digits, k=8))
-        db.session.add(UserLogin(login, group, password))
-        students.write("{}\t{}\t{}\n".format(login, password, line))
-db.session.commit()
-
-for context in content['contexts']:
-    items = list(context.items())[0]
-    if isinstance(items[1][0], str):
-        bs = bytes(dumps(items[1]), "utf8")
-    else:
-        merged = {}
-        for i in items[1]:
-            merged.update(i)
-        bs = bytes(dumps(merged), "utf8")
-    db.session.add(Context(items[0], bs))
-db.session.commit()
-
-counter = 1
-for exercise in content['exercises']:
-    db.session.add(Exercise(exercise['url'], counter))
-    groups = exercise['groups']
-    limit = counter + len(groups) - 1
-    for group in groups:
-        if counter == limit:
-            next = None
+def gen_contexts(contexts):
+    for context in contexts:
+        items = list(context.items())[0]
+        if isinstance(items[1][0], str):
+            bs = bytes(dumps(items[1]), "utf8")
         else:
-            next = counter + 1
-        for task in group:
-            item = Task(counter, task, next)
-            db.session.add(item)
-            db.session.commit()
-            item.parse_formulae()
-            db.session.commit()
-        counter += 1
-db.session.commit()
+            merged = {}
+            for i in items[1]:
+                merged.update(i)
+            bs = bytes(dumps(merged), "utf8")
+        db.session.add(Context(items[0], bs))
+    db.session.commit()
+
+def gen_exercises(exercises):
+    counter = 1
+    for exercise in exercises:
+        db.session.add(Exercise(exercise['url'], counter))
+        groups = exercise['groups']
+        limit = counter + len(groups) - 1
+        for group in groups:
+            if counter == limit:
+                next = None
+            else:
+                next = counter + 1
+            for task in group:
+                item = Task(counter, task, next)
+                db.session.add(item)
+                db.session.commit()
+                item.parse_formulae()
+                db.session.commit()
+            counter += 1
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    content = load(open("state.yaml"))
+    students = open("students.txt", "r")
+    db.session.add(UserLogin("guest", "anonymous", "guest"))
+    db.session.commit()
